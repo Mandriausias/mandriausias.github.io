9a9cb9241162b3ab8ca3f7e646d99807fdebb868
Darius Aleksiunas <aleksiuno@gmail.com>
2024-05-02 23:03:53 +0300

    implement initial nextjs app

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import './globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Homepage',
+  description: 'Initial page description'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
