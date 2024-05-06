431461760f7d857d199bf1b611851fe15f824b38
Darius Aleksiunas <aleksiuno@gmail.com>
2024-05-06 23:45:55 +0300

    add demo components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Montserrat } from 'next/font/google'
+import { Montserrat, Roboto_Mono } from 'next/font/google'
+import NavBar from '@/components/nav/NavBar'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
+const robotoMono = Roboto_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Homepage',
@@ -16,7 +18,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="dracula">
-      <body className={`prose ${montserrat.className}`}>
+      <body className={`prose ${robotoMono.className} card w-screen min-w-fit max-w-full content-center lg:mx-auto`}>
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
