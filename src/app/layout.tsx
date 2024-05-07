25aa16276462f0f1b88516be5cf14b15384d23ad
mandriausias <45068467+Mandriausias@users.noreply.github.com>
2024-05-07 22:36:34 +0300

    Feature/implement-homepage (#2)
    * add daisy ui lib
    
    * add dracula theme
    
    * copy sample workflow from gitgub actions repo
    
    * add demo components
    
    * implement standard navbar
    
    ---------
    
    Co-authored-by: Darius Aleksiunas <aleksiuno@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import './globals.css'
-import { Montserrat } from 'next/font/google'
+import { Montserrat, Roboto_Mono } from 'next/font/google'
+import NavBar from '@/components/nav/NavBar'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
+const robotoMono = Roboto_Mono({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Homepage',
@@ -15,8 +17,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={montserrat.className}>
+    <html lang="en" data-theme="dracula">
+      <body className={`prose ${robotoMono.className} card w-screen min-w-fit max-w-full content-center lg:mx-auto`}>
+        <NavBar />
         <main>{children}</main>
       </body>
     </html>
